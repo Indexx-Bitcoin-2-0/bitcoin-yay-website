57b56b5d72d214bc041f8de98adf00ed2ff038e2
Zain-Razzaq <zainrazzaq2003@gmail.com>
2025-07-19 11:24:51 +0500

    Added the retained BTCY page

---
+++ src/app/alchemy/retained/page.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+
+import ArtImage1 from "@/assets/images/alchemy/retained/art-1.webp";
+
+import RetryButtonImage from "@/assets/images/buttons/retry-button.webp";
+import StarButtonImage from "@/assets/images/buttons/star-button.webp";
+
+import CustomButton2 from "@/components/CustomButton2";
+
+export default function CongratulationsPage() {
+  return (
+    <div className="mx-auto mt-20 lg:mt-60">
+      <Image
+        src={ArtImage1}
+        alt="Art 1"
+        className="w-60 md:w-80 xl:w-120 absolute top-40 left-20"
+      />
+
+      <div className="mt-120 lg:mt-80 flex flex-col items-center justify-center">
+        <h2 className=" text-8xl font-bold">Ops!</h2>
+        <h2 className="mt-20 text-4xl md:text-6xl">You retained</h2>
+        <p className="mt-10 text-7xl md:text-[160px] font-bold">
+          1,000 <span className="text-5xl md:text-8xl">BTCY</span>
+        </p>
+      </div>
+      <div className="mt-20 xl:mt-40 mb-40 flex items-center justify-center gap-40 px-10">
+        <CustomButton2
+          image={RetryButtonImage}
+          text="Re-enter"
+          link="#"
+          imageStyling="w-30"
+        />
+        <CustomButton2
+          image={StarButtonImage}
+          text="Claim"
+          link="#"
+          imageStyling="w-30"
+        />
+      </div>
+    </div>
+  );
+}
