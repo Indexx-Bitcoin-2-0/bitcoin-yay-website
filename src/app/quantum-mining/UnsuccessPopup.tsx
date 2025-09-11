eec83822e4e53c454f81b7493ca908665a39a1c0
Zain-Razzaq <zainrazzaq2003@gmail.com>
2025-09-11 21:17:44 +0500

    Udpated graphics on quantum mining page

--- src/app/quantum-mining/UnsuccessPopup.tsx
+++ src/app/quantum-mining/UnsuccessPopup.tsx
@@ -1,8 +1,9 @@
-"use client";
+import Image from "next/image";
 
 import PopupComponent from "@/components/PopupComponent";
 import CustomButton2 from "@/components/CustomButton2";
 
+import CrossImage from "@/assets/images/icons/cross-icon.webp";
 import RetryButtonImage from "@/assets/images/buttons/retry-button.webp";
 
 export default function UnsuccessPopup({
@@ -16,7 +17,8 @@ export default function UnsuccessPopup({
     <div>
       <PopupComponent isOpen={isOpen} onClose={onClose}>
         <div className="w-90 md:w-120 lg:w-140 p-4 md:p-6 xl:p-10 text-center">
-          <div>
+          <div className="flex flex-col items-center justify-center">
+            <Image src={CrossImage} alt="Close" className="w-24 h-24" />
             <h1 className="text-2xl md:text-3xl font-bold">
               Payment Unsuccessful!
             </h1>
